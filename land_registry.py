055a4dc29f5f16dc660bc9b5ff94bf44b591610a
Anderson <peanderson@ea.com>
2018-12-11 20:48:08 +0000

    Adding some converter - need to figure out a better way of doing this

--- land_registry.py
+++ land_registry.py
@@ -33,7 +33,28 @@ class land_registry_entry:
 
     def output(self):
         print("type: {}".format(self.property_type))
+        print("\tpaon: {}".format(self.paon))
         print("\tprice_paid: {}".format(self.price_paid))
+        print("\testate_type: {}".format(self.estate_type))
+        print("\ttransaction_date: {}".format(self.transaction_date))
+
+def get_estate_type_name(estate_type):
+    """
+    Convert the single letter to a full word
+    """
+    estate_type_name = estate_type
+    if ("F" == estate_type):
+        estate_type_name = "Freehold"
+    return estate_type_name
+
+def get_property_type_name(property_type):
+    """
+    Convert the single letter to a full word
+    """
+    property_type_name = property_type
+    if ("T" == property_type):
+        property_type_name = "Terraced"
+    return property_type_name
 
 def get_additional_prices_paid(street_name, town):
     """
@@ -51,20 +72,20 @@ def get_additional_prices_paid(street_name, town):
         transaction_id = parts[0]
         item_url = parts[15]
         lr_entry = land_registry_entry(transaction_id, item_url)
-        lr_entry.price_paid = parts[1]
-        lr_entry.transaction_date = parts[2]
-        lr_entry.postcode = parts[3]
-        lr_entry.property_type = parts[4]
-        lr_entry.new_build = parts[5]
-        lr_entry.estate_type = parts[6]
-        lr_entry.saon = parts[7]
-        lr_entry.paon = parts[8]
-        lr_entry.street = parts[9]
-        lr_entry.locality = parts[10]
-        lr_entry.town = parts[11]
-        lr_entry.district = parts[12]
-        lr_entry.county = parts[13]
-        lr_entry.record_status = parts[14]
+        lr_entry.price_paid = parts[1].replace("\"","")
+        lr_entry.transaction_date = parts[2].replace("\"","")
+        lr_entry.postcode = parts[3].replace("\"","")
+        lr_entry.property_type = get_property_type_name(parts[4].replace("\"",""))
+        lr_entry.new_build = parts[5].replace("\"","")
+        lr_entry.estate_type = get_estate_type_name(parts[6].replace("\"",""))
+        lr_entry.saon = parts[7].replace("\"","")
+        lr_entry.paon = parts[8].replace("\"","")
+        lr_entry.street = parts[9].replace("\"","")
+        lr_entry.locality = parts[10].replace("\"","")
+        lr_entry.town = parts[11].replace("\"","")
+        lr_entry.district = parts[12].replace("\"","")
+        lr_entry.county = parts[13].replace("\"","")
+        lr_entry.record_status = parts[14].replace("\"","")
         transations.append(lr_entry)
 
     return transations
